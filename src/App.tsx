bc94d9e0680060b7bc3b0297ee3a93078e9c19f7
Sebastian Chacon <nonexistentexistentialist1@gmail.com>
2025-07-17 11:09:11 -0500

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import { Button } from './components/ui/button'
+import { ThemeProvider } from './components/theme-provider'
+import { ModeToggle } from './components/mode-toggle'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <ModeToggle />
+    <ThemeProvider  defaultTheme="light" storageKey="vite-ui-theme">
       <div className="flex min-h-svh flex-col items-center justify-center">
       <Button onClick={() => setCount((count) => count + 1)}>Click me: {count}</Button>
     </div>
+    </ThemeProvider>
     </>
   )
 }
